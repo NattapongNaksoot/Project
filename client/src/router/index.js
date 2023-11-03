c9204d435b323b82863197bef543b039b3f4de78
NattapongNaksoot <139940383+NattapongNaksoot@users.noreply.github.com>
2023-11-04 03:10:52 +0700

    final

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,30 +4,6 @@ import Router from 'vue-router'
 // Index
 import Index from '@/components/biomes/Index'
 
-// Electrolux
-import ElectroluxIndex from '@/components/biomes/Index/IndexElectrolux'
-import ElectroluxCreate from '@/components/biomes/Create/CreateElectrolux'
-import ElectroluxEdit from '@/components/biomes/Edit/EditElectrolux'
-import ElectroluxShow from '@/components/biomes/Show/ShowElectrolux'
-
-// Philips
-import PhilipsIndex from '@/components/biomes/Index/IndexPhilips'
-import PhilipsCreate from '@/components/biomes/Create/CreatePhilips'
-import PhilipsEdit from '@/components/biomes/Edit/EditPhilips'
-import PhilipsShow from '@/components/biomes/Show/ShowPhilips'
-
-// Tefal
-import TefalIndex from '@/components/biomes/Index/IndexTefal'
-import TefalCreate from '@/components/biomes/Create/CreateTefal'
-import TefalEdit from '@/components/biomes/Edit/EditTefal'
-import TefalShow from '@/components/biomes/Show/ShowTefal'
-
-// Xiaomi
-import XiaomiIndex from '@/components/biomes/Index/IndexXiaomi'
-import XiaomiCreate from '@/components/biomes/Create/CreateXiaomi'
-import XiaomiEdit from '@/components/biomes/Edit/EditXiaomi'
-import XiaomiShow from '@/components/biomes/Show/ShowXiaomi'
-
 //Aqatic
 import AquaticIndex from '@/components/biomes/Index/IndexAquatic'
 import AquaticCreate from '@/components/biomes/Create/CreateAquatic'
@@ -69,95 +45,6 @@ export default new Router({
       name: 'index',
       component: Index
     },
-
-    // Electrolux
-    {
-      path: '/electroluxs',
-      name: 'electroluxs',
-      component: ElectroluxIndex
-    },
-    {
-      path: '/electrolux/create',
-      name: 'electrolux-create',
-      component: ElectroluxCreate
-    },
-    {
-      path: '/electrolux/edit/:electroluxId',
-      name: 'electrolux-edit',
-      component: ElectroluxEdit
-    },
-    {
-      path: '/electrolux/:electroluxId',
-      name: 'electrolux',
-      component: ElectroluxShow
-    },
-
-    // Philips
-    {
-      path: '/philipss',
-      name: 'philipss',
-      component: PhilipsIndex
-    },
-    {
-      path: '/philips/create',
-      name: 'philips-create',
-      component: PhilipsCreate
-    },
-    {
-      path: '/philips/edit/:philipsId',
-      name: 'philips-edit',
-      component: PhilipsEdit
-    },
-    {
-      path: '/philips/:philipsId',
-      name: 'philips',
-      component: PhilipsShow
-    },
-
-    // Tefal
-    {
-      path: '/tefals',
-      name: 'tefals',
-      component: TefalIndex
-    },
-    {
-      path: '/tefal/create',
-      name: 'tefal-create',
-      component: TefalCreate
-    },
-    {
-      path: '/tefal/edit/:tefalId',
-      name: 'tefal-edit',
-      component: TefalEdit
-    },
-    {
-      path: '/tefal/:tefalId',
-      name: 'tefal',
-      component: TefalShow
-    },
-
-    // Xiaomi
-    {
-      path: '/xiaomis',
-      name: 'xiaomis',
-      component: XiaomiIndex
-    },
-    {
-      path: '/xiaomi/create',
-      name: 'xiaomi-create',
-      component: XiaomiCreate
-    },
-    {
-      path: '/xiaomi/edit/:xiaomiId',
-      name: 'xiaomi-edit',
-      component: XiaomiEdit
-    },
-    {
-      path: '/xiaomi/:xiaomiId',
-      name: 'xiaomi',
-      component: XiaomiShow
-    },
-
 
     //Aquatic//
     {
